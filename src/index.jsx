01409ee8a51f69daa44f6c0809b7f487e523732c
Álvaro Cuesta <alvaro@redradix.com>
2018-08-18 14:47:32 +0200

    avoid too long lines

--- src/index.jsx
+++ src/index.jsx
@@ -43,14 +43,31 @@ class SignUpForm extends React.Component {
     }
 
     render() {
-        const url = this.generateURL(this.state.width, this.state.height, this.state.category, this.state.filter);
+        const url = this.generateURL(
+            this.state.width,
+            this.state.height,
+            this.state.category,
+            this.state.filter
+        );
 
         return (
             <div>
-                <input type="number" onChange={this.handleWidthChange.bind(this)} value={this.state.width}/>
-                <input type="number" onChange={this.handleHeightChange.bind(this)} value={this.state.height}/>
+                <input
+                    type="number"
+                    onChange={this.handleWidthChange.bind(this)}
+                    value={this.state.width}
+                />
+                <input
+                    type="number"
+                    onChange={this.handleHeightChange.bind(this)}
+                    value={this.state.height}
+                />
 
-                <select name="category" onChange={this.handleCategoryChange.bind(this)} value={this.state.category}>
+                <select
+                    name="category"
+                    onChange={this.handleCategoryChange.bind(this)}
+                    value={this.state.category}
+                >
                     <option value="any">Any</option>
                     <option value="animals">Animals</option>
                     <option value="arch">Architecture</option>
@@ -60,9 +77,38 @@ class SignUpForm extends React.Component {
                 </select>
 
                 <div id="filter">
-                    <label><input type="radio" name="filter" value="undefined" checked={this.state.filter==="undefined"} onChange={this.handleFilterChange.bind(this)}/> No filter</label>
-                    <label><input type="radio" name="filter" value="grayscale" checked={this.state.filter==="grayscale"} onChange={this.handleFilterChange.bind(this)}/> Grayscale</label>
-                    <label><input type="radio" name="filter" value="sepia" checked={this.state.filter==="sepia"} onChange={this.handleFilterChange.bind(this)}/> Sepia</label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="filter"
+                            value="undefined"
+                            checked={this.state.filter==="undefined"}
+                            onChange={this.handleFilterChange.bind(this)}
+                        />
+                        No filter
+                    </label>
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="filter"
+                            value="grayscale"
+                            checked={this.state.filter==="grayscale"}
+                            onChange={this.handleFilterChange.bind(this)}
+                        />
+                        Grayscale
+                    </label>
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="filter"
+                            value="sepia"
+                            checked={this.state.filter==="sepia"}
+                            onChange={this.handleFilterChange.bind(this)}
+                        />
+                        Sepia
+                    </label>
                 </div>
 
                 <img src={url} />
